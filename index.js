a46d893d24dce02b2689aac2e41badeb95d7a483
CharanyaR <ravichandran.charanya@gmail.com>
2016-11-19 23:30:05 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+  
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: '5f4da2a9-e58c-4711-b8ad-f2eb72bb91bd',
+    appPassword: 'zOXoL6JsdN6Khkc28b9QqXL'
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+bot.dialog('/', function (session) {
+    session.send("Hello World");
+});
